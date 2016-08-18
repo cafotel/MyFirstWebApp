f7794840de1d51fcf77fd0b65268fc0ce82fc4ec
cafotel <cafotel@configit.com>
2016-08-18 11:27:12 +0200

    Put method implemented

--- MyFirstWebApp/Controllers/ConfigurationController.cs
+++ MyFirstWebApp/Controllers/ConfigurationController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -6,11 +7,13 @@ using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Web.Helpers;
 using System.Web.Http;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using Configit.Base;
 using Configit.Base.Collections;
 using Configit.Core.Capabilities.Converters;
 using Configit.Runtime;
@@ -20,27 +23,31 @@ using Newtonsoft.Json.Linq;
 
 namespace MyFirstWebApp.Controllers {
 
-  public class ConfigVariables {
+  public class ConfigVariable {
 
-    public ConfigVariables( int index, string name, string showas, List<ConfigValues> values  ) {
+    public ConfigVariable( Guid id, int index,  string name, string showas, List<ConfigValue> values  ) {
+      Id = id;
       Index = index;
       Name = name;
       ShowAs = showas;
       ConfigValues = values;
     }
+    public Guid Id { get; set; }
     public int Index { get; set; }
     public string Name { get; set; }
     public string ShowAs { get; set; }
-    public List<ConfigValues> ConfigValues { get; set; }
+    public List<ConfigValue> ConfigValues { get; set; }
   }
 
-  public class ConfigValues {
-    public ConfigValues(string name, string fullName, string state) {
+  public class ConfigValue {
+    public ConfigValue(Guid id, string name, string fullName, string state) {
+      Id = id;
       Name = name;
       FullName = fullName;
       State = state;
     }
 
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string FullName { get; set; }
     public string State { get; set; }
@@ -55,14 +62,19 @@ namespace MyFirstWebApp.Controllers {
       _model = ProductModel.CreateFromVT( path );
     }
 
-    public IEnumerable<ConfigVariables> Get() {
+    // TODO: maybe change this into an IHTTPActionResult, so as to avoid the ConfigVariable and ConfigValue classes
+    [HttpGet]
+    public IEnumerable<ConfigVariable> Get() {
       var variables = _model.AllVariables;
 
-      var returnVariables = variables.Select( v => new ConfigVariables(
+      var returnVariables = variables.Select( v => new ConfigVariable(
+
+                                                v.ElementUuid,
                                                 v.Index,
                                                 v.Name,
                                                 v.ShowAs,
-                                                v.Values.Select( x => new ConfigValues(
+                                                v.Values.Select( x => new ConfigValue(
+                                                                    x.ElementUuid,
                                                                     x.Name,
                                                                     x.FullyQualifiedName,
                                                                     v.GetState( x ).ToString()
@@ -74,26 +86,58 @@ namespace MyFirstWebApp.Controllers {
 
     // GET: api/Configuration/5
     /* returns the values of a given variable including the value states */
-    public IHttpActionResult Get( int id ) {
+    [HttpGet]
+    public IHttpActionResult GetValues( int id ) {
 
-      // select the values of the variable given in the input parameter
+      // avoid out of bounds exception
+      if( id >= _model.AllVariables.Count ) {
+        return NotFound();
+      }
+
       var variable = _model.AllVariables[id];
       var values = variable.Values;
-      var vars = values.Select( v => new { v.Name,
-                                           FullName = v.FullyQualifiedName,
-                                           State = variable.GetState(v) } ).ToList();
 
-      return Ok( vars );
+      var valueResult = values.Select( v => new {
+                                      Uuid = v.ElementUuid,
+                                      v.Name,
+                                      v.FullyQualifiedName,
+                                      State = variable.GetState( v )
+      } ).ToList();
 
-
-   }
+      return Ok( valueResult );
+    }
 
     // POST: api/Configuration
     public void Post( [FromBody]string value ) {
+
     }
 
-    // PUT: api/Configuration/5
-    public void Put( int id, [FromBody]string value ) {
+    // PUT: api/Configuration/
+    [HttpPut]
+    public IHttpActionResult Put( List<Assignment> assignments, bool force = false ) {
+
+      // apply assigned value to model
+      _model.ApplyAssignments(assignments);
+
+      var result = new object();
+      // check the model for conflicts
+      if ( !_model.HasConflict ) {
+        return Ok( new { HasConflict = _model.HasConflict } );
+      }
+
+      var conflict = _model.GetConflict();
+      return Ok( new {
+        HasConflict = _model.HasConflict,
+        HeaderText = conflict.HeaderText,
+        Message = conflict.Message,
+        ForceText = conflict.ForceText,
+        UndoText = conflict.UndoText
+      } );
+
+    }
+
+    public IHttpActionResult ForceAssignment() {
+      return Ok(Get());
     }
 
     // DELETE: api/Configuration/5
